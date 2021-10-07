2e503c64b90b0a3e66a4c500e1d239727c981509
Rob Cone <robcone86@gmail.com>
2021-10-07 13:26:01 -0500

    working on ajax post

---
+++ server/modules/playerNames.js
@@ -0,0 +1,19 @@
+const playerName = [{
+        name: 'Rob',
+        number: '2'
+    },
+    {
+        name: 'Julia',
+        number: '3'
+    },
+    {
+        name: 'Jacob',
+        number: '4'
+    },
+    {
+        name: 'Emily',
+        number: '10'
+    },
+];
+
+module.exports = playerName;
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,24 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("jquery is loaded!")
+    console.log("jquery is loaded!")
+}
+
+function getGuesses() {
+    $.ajax({
+        type: 'GET',
+        url: '/playerName'
+    }).then(function(response) {
+        console.log('successful response', response);
+        //add render function later
+    }).catch(function(response) {
+        alert('Im broken!!!!! :(');
+    })
+
+}
+
+function postNewGuesses() {
+    $.ajax({
+        type: 'POST',
+    })
 }
